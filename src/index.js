0bca045bac3f85bf7b4a59d2feda89b8f1efff32
Moureen <moureenham@gmail.com>
2021-12-12 17:06:14 +0530

    binding app with redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
 
 import App from "./components/App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStore(reducers, {})}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById("root")
 );
 
