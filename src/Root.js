92158167f3bf37478c9560b42bbf84004be57c45
Moureen <moureenham@gmail.com>
2022-01-16 01:29:41 +0530

    middleware

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware } from "redux";
 import reduxPromise from "redux-promise";
 import reducers from "./reducers";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
