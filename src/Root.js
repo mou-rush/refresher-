b3a981407201321d7cfdbca1ee7750a2f4e96c4a
Moureen <moureenham@gmail.com>
2022-01-29 03:26:38 +0530

    Emitting Warnings

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,17 @@
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
+// import reduxPromise from "redux-promise";
+import async from "./middlewares/async";
 import reducers from "./reducers";
+import stateValidator from "./middlewares/stateValidator";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidator)
   );
+
   return <Provider store={store}>{children}</Provider>;
 };
